7842fa4364e7b459c6b2e31fe88d97020162fa02
DogRacer-Laptop <doggieracer@hotmail.com>
2023-03-02 17:59:48 +0000

    Cleaned up App.py

--- App.py
+++ App.py
@@ -2,25 +2,7 @@ def start_up():
     print("Hello I am starting");
 
 
-def otherFunc(name):
-    nums = []
-    list(nums).append("This is in the array")
-    print(nums[0])
-    print(name)
-
-def someOtherFunc():
-    myList = []
-    list(myList).append("Hello world")
-    list(myList).append("Jack")
-    list(myList).append("Ben")
-    list(myList).append("James")
-
-    for y in myList:
-        print(y)
-
-
 def output_click_testBtn():
-    # will convert from celcius to farenheit
     __COMMENT__("This function will take input from the input withid of 'testInput' and will print a fahrenheit version of the temperature to the console will also contain error checking")
     celcius = getValue_testInput()
     if (celcius != ""):
@@ -30,10 +12,3 @@ def output_click_testBtn():
         setOutput_outputTest(fahrenheit)
     else:
         print("You need to enter a number")
-
-
-def testClickOfInput_click_testInput():
-    #num = getValue_testInput()
-    print("The value in the input is as below")
-    #print(getValue_testInput())
-    __COMMENT__("THIS IS MY TEST COMMENT I ADDED IN PYTHON")
